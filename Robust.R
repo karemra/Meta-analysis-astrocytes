dce0ecec06f99a6cee9dce440c21a9432d99d53f
Karem_ra <ruizkarem@javeriana.edu.co>
2026-02-07 17:06:33 -0500

    Update Robust.R

--- Robust.R
+++ Robust.R
@@ -1,6 +1,5 @@
-install.packages("RobustRankAggreg")
+if (!require("RobustRankAggreg")) install.packages("RobustRankAggreg")
 library(RobustRankAggreg)
-
 meta1<- read.delim("MatrizDOWN.txt",na="")
 
 #Convert the data to a suitable format. RobustRankAggreg package needs the input to be 
@@ -21,13 +20,12 @@ for (i in 1: length(meta)){
 
 count <- c(61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228,61228) 
 
-#count <- c(1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976,1976) 
-
 ##Construct the matrix of normalized ranks using rankMatrix command:
-## The argument N is used to define the number of miRNAs each study was able to profile (previously defined vector count)
+## The argument N is used to define the number of genes each study was able to profile (previously defined vector count)
 rankmat <- rankMatrix(meta, N = count) 
 
-##Run RRA analysis using the “aggregateRanks” command to obtain a significance score
+##Run RRA analysis using the “aggregateRanks” command to obtain a significance score, 
+##which identifies genes ranked consistently higher than expected by chance
 
 ranks<- aggregateRanks(rmat=rankmat)  
 
@@ -39,10 +37,11 @@ ranks$adjustedPval <-  apply(cbind(ranks$Score
                                    * max(count), 1), 1, min) 
 
 ##Filter the results by significance, This command allows you to construct the new data
-##frame results, which consists of data about only those miRNAs with adjusted p-value < 0.05
+##frame results, which consists of data about only those genes with adjusted p-value < 0.05
 
 results <- ranks[ranks$adjustedPval < 0.05,] 
 
 ##Export the results table out of R with the following command
 write.table(results, "results_down.txt", sep = "\t") 
+
 
